4354cab30c63bd3e34213df842daed1b6d86c20c
Latifa <latimess@yahoo.fr>
2018-05-20 18:43:05 -0400

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://latimess:Soumia75$@ds127883.mlab.com:27883/todoapp');
 module.exports = {mongoose};
