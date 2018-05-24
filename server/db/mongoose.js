cdead7d645e7f9b80d04e55f401e2571e9411a4a
Latifa <latimess@yahoo.fr>
2018-05-24 15:52:38 -0400

    Add PATCH /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-// mongoose.connect('mongodb://ds127883.mlab.com:27883/todoapp',
-// {
-//     auth: {
-//       user: 'latimess',
-//       password: 'Soumia75$'
-//     }
-//   });
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://ds127883.mlab.com:27883/todoapp',
+{
+    auth: {
+      user: 'latimess',
+      password: 'Soumia75$'
+    }
+  });
 module.exports = {mongoose};
